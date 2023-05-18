4e232515869dd8666ef5101fbd1b18260888281d
SamueleA <batonenbois@gmail.com>
2023-05-18 16:50:38 -0400

    workaround for wagmi more similar to official example

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,12 @@
 import '../styles/global.css';
 import type { AppProps } from 'next/app';
-import dynamic from 'next/dynamic'
+import WagmiProvider from '../components/WagmiProvider';
 
 import {
   ThemeProvider,
 } from '@0xsequence/design-system'
 
 import '@0xsequence/design-system/styles.css'
-
-const WagmiProvider = dynamic(() => import('./WagmiProvider'), {
-  ssr: false
-})
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
